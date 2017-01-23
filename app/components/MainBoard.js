e649d47600242bb4fb4f0eb3e2babffd1180854e
travor <travor20814@gmail.com>
2017-01-23 20:01:29 +0800

    add android drawer, list button, picker

--- app/components/MainBoard.js
+++ app/components/MainBoard.js
@@ -1,8 +1,17 @@
 import {
   StyleSheet,
   View,
+  Text,
+  DrawerLayoutAndroid,
+  ListView,
+  Picker,
+  Button,
+  Alert,
 } from 'react-native';
-import React from 'react';
+import React, {
+	Component,
+	PropTypes as T,
+} from 'react';
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -10,18 +19,65 @@ const styles = StyleSheet.create({
     backgroundColor: '#f6f6f6',
     alignSelf: 'stretch',
   },
+  drawerList: {
+    backgroundColor: '#9b9b9b',
+  },
+  listItem: {
+    color: '#a52a2a',
+    marginBottom: 10,
+  },
 });
 
-function MainBoard(props) {
-  const {
-    children,
-  } = props;
+class MainBoard extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
-  return (
-    <View style={styles.wrapper}>
-      {children}
-    </View>
-  );
+    this.state = {
+      dataSource: ds.cloneWithRows(['Button 1', 'Button 2']),
+      language: 'java',
+    };
+  }
+
+  onPressDrawerButton() {
+    Alert.alert('Button has been pressed!');
+  }
+
+  render() {
+    const {
+      children,
+    } = this.props;
+
+    var navigationView = (
+      <View style={{flex: 1, backgroundColor: '#fff'}}>
+        <ListView
+          style={styles.drawerList}
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Button
+            onPress={() => this.onPressDrawerButton()}
+            style={styles.listItem}
+            title={rowData}
+            accessibilityLabel="Learn more" />} />
+          <Picker
+            selectedValue={this.state.language}
+            onValueChange={(lang) => this.setState({language: lang})}>
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+          </Picker>
+      </View>
+    );
+
+    return (
+      <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => navigationView}>
+        <View style={styles.wrapper}>
+          {children}
+        </View>
+      </DrawerLayoutAndroid>
+    );
+  }
 }
 
 export default MainBoard;
