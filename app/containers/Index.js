11446d12825f983ec8ef88fc4cb1258ab1c4d6f3
travor <travor20814@gmail.com>
2017-01-26 13:56:18 +0800

    Modal tag practice

--- app/containers/Index.js
+++ app/containers/Index.js
@@ -7,7 +7,9 @@ import {
 	Text,
 	Alert,
 	TouchableOpacity,
+	TouchableHighlight,
 	ToastAndroid,
+	Modal,
 } from 'react-native';
 import Login from './Login';
 
@@ -28,6 +30,7 @@ class Index extends Component {
 
 		this.state = {
 			status: 0,
+			modalVisible: false,
 		};
 	}
 
@@ -45,6 +48,10 @@ class Index extends Component {
     ]);
 	}
 
+	setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
 	render() {
 		return (
 			<View style={styles.wrapper}>
@@ -55,6 +62,30 @@ class Index extends Component {
 				<TouchableOpacity onPress={() => this.alertDialog()}>
 					<Text style={styles.newPage}>Jump alert</Text>
 				</TouchableOpacity>
+				<TouchableHighlight
+					underlayColor='#4F20A4'
+					onPress={() => this.openPage()}>
+					<Text style={styles.newPage}>Login high</Text>
+				</TouchableHighlight>
+				<View style={{ marginTop: 22 }}>
+	        <TouchableHighlight onPress={() => { this.setModalVisible(true) }}>
+	          <Text>Show Modal</Text>
+	        </TouchableHighlight>
+					<Modal
+	          animationType={"fade"}
+	          transparent={false}
+	          visible={this.state.modalVisible}
+	          onRequestClose={() => { alert("Modal has been closed.") }}>
+	         <View style={{ marginTop: 22 }}>
+	          <View>
+	            <Text>Hello World!</Text>
+	            <TouchableHighlight onPress={() => { this.setModalVisible(!this.state.modalVisible) }}>
+	              <Text>Hide Modal</Text>
+	            </TouchableHighlight>
+	          </View>
+	         </View>
+	        </Modal>
+	      </View>
 			</View>
 		);
 	}
